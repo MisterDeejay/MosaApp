118a17babe9a14960e030991ea0fd5192bbe594e
Khaaliq DeJan <khaaliqdejan@gmail.com>
2015-02-03 14:11:41 -0800

    Add user model and controller

--- mosa_app/app/assets/javascripts/routers/mosa_router.js
+++ mosa_app/app/assets/javascripts/routers/mosa_router.js
@@ -11,4 +11,14 @@ window.Mosa.Routers.MosaRouter = Backbone.Router.extend({
 	restaurantsShow: function(id) {
 		
 	},
+	
+	_swampView: function(view) {
+		// stopListening() removes events on a view
+		if(this.currentView) {
+			this.currentView.remove();
+		}
+		this.currentView = view;
+
+		$("body").html(view.render().$el);
+	}
 })
